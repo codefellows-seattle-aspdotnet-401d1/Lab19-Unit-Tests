dabdfd36af98aa6a67af213723b7d77dfa8463d8
Brandon Little <brlittle86@gmail.com>
2017-10-09 23:07:54 -0700

    created test repo, implementing IRepository and using test data

---
+++ lab19-brandon/PackerTracker/PackerTracker/Models/TestDestinationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PackerTracker.Models
+{
+    public class TestDestinationRepository : IRepository
+    {
+        public IEnumerable<Destination> Destination => new List<Destination>
+        {
+            new Destination{LocationName = "Hawaii", Country = "USA", RequiredItems = new List<string>{"Shorts", "Sandals", "Beach Towels", "Sunscreen"}},
+            new Destination{LocationName = "Dublin", Country = "Ireland", RequiredItems = new List<string>{"Raincoat", "Identification", "Castle Guidebook"}},
+            new Destination{LocationName = "Tokyo", Country = "Japan", RequiredItems = new List<string>{"Clean White Socks", "Umbrella", "Adventurous Appetite"}}
+        };
+    }
+}
