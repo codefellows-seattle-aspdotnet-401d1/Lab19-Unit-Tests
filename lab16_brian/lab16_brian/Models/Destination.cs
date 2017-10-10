eefadf458328388c5fd7aa8351c96eb86835e1db
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-09 21:07:10 -0700

    Inserted Lab 16 to test. Created test folder and new test solution

---
+++ lab16_brian/lab16_brian/Models/Destination.cs
@@ -0,0 +1,11 @@
+﻿namespace lab16_brian.Models
+{
+    public class Destination
+    {
+        public int ID { get; set; }
+        public string Dest { get; set; }
+        public string Footware { get; set; }
+        public bool Swimwwear { get; set; }
+
+    }
+}
---
+++ lab16_brian/lab16_brian/Models/IDestinations.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace lab16_brian.Models
+{
+    public interface IDestinations
+    {
+        IEnumerable<Destination> Destinations { get; }
+    }
+}
