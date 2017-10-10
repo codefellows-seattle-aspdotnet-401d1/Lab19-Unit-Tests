a86233093d84b6bffba6d2b62157ee630f5a317b
Brandon Little <brlittle86@gmail.com>
2017-10-09 23:11:38 -0700

    added Transient to Startup

--- lab19-brandon/PackerTracker/PackerTracker/Startup.cs
+++ lab19-brandon/PackerTracker/PackerTracker/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PackerTracker.Models;
 
 namespace PackerTracker
 {
@@ -18,6 +19,7 @@ namespace PackerTracker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IRepository, TestDestinationRepository>();
             services.AddMvc();
         }
 
