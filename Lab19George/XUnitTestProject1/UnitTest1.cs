b57bea1464463297832ac65cba69087a02a0e9e8
George Gaylor <georgegaylor83@outlook.com>
2017-10-16 10:26:48 -0700

    Added 2 more tests
    Also deleted false test project

--- Lab19George/XUnitTestProject1/UnitTest1.cs
+++ Lab19George/XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Lab19George.Models;
+using Lab19George.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
@@ -17,6 +19,32 @@ namespace XUnitTestProject1
 
             // Assert
             Assert.Equal(c.City, "Tacoma");
+        }
+
+        [Fact]
+        public void CityIsString()
+        {
+            // Arrange
+            var c = new Destination();
+
+            // Act
+            c.City = "Seattle";
+
+            // Assert
+            Assert.IsType<string>(c.City);
+        }
+
+                [Fact]
+        public void HomeIndexResultIsAView()
+        {
+            // Arrange
+            HomeController c = new HomeController();
+
+            // Act
+            IActionResult result = c.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
